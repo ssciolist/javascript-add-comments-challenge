b33ef323b3520febd9785366c9d5c2f02d1a5e7d
Megan Arellano <ssciolist@gmail.com>
2018-06-19 13:45:51 -0600

    functional add comment feature

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  // add your code here
-})
+  $("#new_comment_button").on( "click", function() {
+    $(this).hide();
+    $("#new_comment").show();
+  });
+
+  $("#new_comment").submit(function() {
+    event.preventDefault()
+    let commentBody = $('textarea[name="comment"]').val()
+    let commentAuthor = $('input[id="authorName"]').val()
+    $("#comment_list").append("<li>" + commentBody + "<span class='author'>" + commentAuthor +
+    "</span></li>")
+    $('#new_comment').hide()
+  })
+});
